2030c5c4f663cc959119e7b1646c23bf45fd7c4e
Ryan Santoyo <ryansantoyo3@gmail.com>
2025-11-18 15:54:32 +0800

    PO Modules - WIP

---
+++ src/services/poservice.ts
@@ -0,0 +1,6 @@
+import { createCrudService } from "./crudServices";
+import { InventoryStockCreateDto, InventoryStockDto,InventoryStockUpdateDto } from "../types/inventory";
+
+export const inventoryServices = 
+createCrudService<InventoryStockDto, InventoryStockCreateDto, InventoryStockUpdateDto>
+("/inventorystock");
