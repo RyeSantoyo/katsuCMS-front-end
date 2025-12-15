c3aabee4ca7da98bdc18d7c14e4d93b4702487f6
Ryan Santoyo <ryansantoyo3@gmail.com>
2025-12-15 15:47:05 +0800

    Custom Endpoint

--- src/services/poservice.ts
+++ src/services/poservice.ts
@@ -1,6 +1,10 @@
+// import { create } from "domain";
 import { createCrudService } from "./crudServices";
 import { PurchaseOrderDto, PurchaseOrderCreateDto, PurchaseOrderUpdateDto } from "@/types/purchaseorder";
+import { api } from "@/lib/api";
 
-export const poServices = 
-createCrudService<PurchaseOrderDto, PurchaseOrderCreateDto, PurchaseOrderUpdateDto>
-("/purchaseorder");
+export const poServices = {
+...createCrudService<PurchaseOrderDto, PurchaseOrderCreateDto, PurchaseOrderUpdateDto>("purchaseorder"),
+generatePoNumber: () => api.get<{poNumber: string}>("/purchaseorder/generateponumber"),
+};
+
